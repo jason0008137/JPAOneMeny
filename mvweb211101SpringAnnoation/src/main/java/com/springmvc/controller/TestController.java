03b28560a364b714c1521dd6735bdac96e0032c8
CaptainButcher <shouming2000@gmail.com>
2021-11-02 17:20:15 +0800

    .

---
+++ mvweb211101SpringAnnoation/src/main/java/com/springmvc/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class TestController
+{
+	@RequestMapping(value =
+	{ "/Test" }, method = RequestMethod.GET)
+	public String listEmp(ModelMap model)
+	{
+		model.addAttribute("emp", "Annie Wang");
+		return "Test";
+	}
+}
