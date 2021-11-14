97890ac79ad8d83f5abda8a83ddd099aa1683451
CaptainButcher <shouming2000@gmail.com>
2021-11-14 18:21:21 +0800

    .

--- mvweb211103ShopShop/src/main/java/model/Product.java
+++ mvweb211103ShopShop/src/main/java/model/Product.java
@@ -1,47 +1,66 @@
 package model;
-public class Product {
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "mvproduct")
+public class Product
+{
+
+	@Id
 	private String id;
 	private String name;
 	private String photo;
 	private double price;
 
-	public String getId() {
+	public String getId()
+	{
 		return id;
 	}
 
-	public void setId(String id) {
+	public void setId(String id)
+	{
 		this.id = id;
 	}
 
-	public String getName() {
+	public String getName()
+	{
 		return name;
 	}
 
-	public void setName(String name) {
+	public void setName(String name)
+	{
 		this.name = name;
 	}
 
-	public String getPhoto() {
+	public String getPhoto()
+	{
 		return photo;
 	}
 
-	public void setPhoto(String photo) {
+	public void setPhoto(String photo)
+	{
 		this.photo = photo;
 	}
 
-	public double getPrice() {
+	public double getPrice()
+	{
 		return price;
 	}
 
-	public void setPrice(double price) {
+	public void setPrice(double price)
+	{
 		this.price = price;
 	}
 
-	public Product() {
+	public Product()
+	{
 	}
 
-	public Product(String id, String name, String photo, double price) {
+	public Product(String id, String name, String photo, double price)
+	{
 		this.id = id;
 		this.name = name;
 		this.photo = photo;
--- mvweb211103ShopShop/src/main/java/model/ProductModel.java
+++ mvweb211103ShopShop/src/main/java/model/ProductModel.java
@@ -3,6 +3,14 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 public class ProductModel
 {
 
@@ -10,11 +18,40 @@ public class ProductModel
 
 	public ProductModel()
 	{
-		this.products = new ArrayList<Product>();
-		this.products.add(new Product("p01", "JBud Elite", "images/p1.jpg", 20));
-		this.products.add(new Product("p02", "EdiMax Wifi", "images/p2.jpg", 21));
-		this.products.add(new Product("p03", "Asus Laptop", "images/p3.jpg", 22));
+		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder()
+				.configure("hibernate.cfg.xml")
+				.build();
 
+		Metadata meta = new MetadataSources(ssr)
+				.getMetadataBuilder()
+				.build();
+
+		SessionFactory factory = meta.getSessionFactoryBuilder().build();
+		Session sessionObj = null;
+		Session session = null;
+		session = factory.openSession();
+		Transaction tx = null;
+
+		try
+		{
+			tx = session.beginTransaction();
+			products = session.createQuery("from Product").list();
+
+			// check----------
+			System.out.println("product list: " + products);
+			// ---------------
+
+			tx.commit();
+		} catch (Exception e)
+		{
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally
+		{
+			session.close();
+			factory.close();
+		}
 	}
 
 	void from_mvDB()
